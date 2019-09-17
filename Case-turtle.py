70fd98d93e5d0e8644b8aa3125920c99dc22f9eb
Vladimir.Silkachev <provovak@yandex.ru>
2019-09-17 11:43:53 +0800

    #1.1

--- Case-turtle.py
+++ Case-turtle.py
@@ -1,1 +1,17 @@
 #Hello guys
+#Developers V.Silkachev
+            #K.Popov
+            #S.Vinnikov
+
+import turtle
+
+def blades():
+    #TODO: (vin):  левый синий треуголльник и правый красный паралледограм
+pass
+
+def mainpart():
+    #TODO: (Kirill): туловище вертолета
+pass
+
+def rhombus()
+    #TODO: (vova)
